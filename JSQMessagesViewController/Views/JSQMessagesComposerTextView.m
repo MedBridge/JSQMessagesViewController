39603f95320bf39e09eec6fbf66b5397dfe6877d
Parker <parker@medbridgeed.com>
2018-01-25 14:54:11 -0800

    rename pasteDelegate to myPasteDelegate

--- JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
+++ JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
@@ -218,7 +218,7 @@
 
 - (void)paste:(id)sender
 {
-    if (!self.pasteDelegate || [self.pasteDelegate composerTextView:self shouldPasteWithSender:sender]) {
+    if (!self.myPasteDelegate || [self.myPasteDelegate composerTextView:self shouldPasteWithSender:sender]) {
         [super paste:sender];
     }
 }
